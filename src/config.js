6d7be3d6e0b597f5eb31a9b5866ef4097bcf445f
Chukwuemeka, Samuel Chinaza <121035920+Smlchinaza@users.noreply.github.com>
2024-08-21 18:18:09 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-module.exports = {telegraf_token:'your telegraf token',
-database:'Your database connection string',
+module.exports = {telegraf_token:'6752968529:AAEjep5PZDdQTgu5yfhYgukk_zA9zhmfV4U',
+database:'mongodb+srv://samchuks898:<q53Yh3lhu4AazhpY>@cluster0.dt5ol.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
 secret:'yoursecret'};
 
